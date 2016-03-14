b2963afbb285dded2f15f716fe138e5b1911f676
Andrew Belyaev <abelyaev.vmk@gmail.com>
2016-03-14 16:07:39 +0300

    lines done

--- ImageProperties.py
+++ ImageProperties.py
@@ -1,4 +1,5 @@
 import numpy as np
+from CommonFunctions import MyDict
 import pickle
 
 
@@ -13,11 +14,24 @@ class ImageObject:
 
 class ImageProperties:
     def __init__(self, project='image'):
-        self.objects = []
+        self.objects = MyDict()
+        self.objects['Ground'] = []
+        self.objects['Walls'] = []
+        self.objects['Sky'] = []
+        self.objects['Motion'] = []
         self.project = project
 
     def add(self, io):
-        self.objects.append(io)
+        self.objects[io.type].append(io.points)
+
+    def __str__(self):
+        ans = ''
+        for key in self.objects:
+            ans += key + ' ' + self.objects[key].__str__() + '\n'
+
+    def copy(self, ip):
+        self.objects = ip.objects
+        self.project = self.project
 
     def save(self, path=None):
         with open(path if path is not None else self.project, 'wb') as f:
@@ -25,5 +39,6 @@ class ImageProperties:
 
     @staticmethod
     def load(path=None):
-        with open(path if path is not None else self.project, 'wb') as f:
-            return pickle.load(f)
+        if path:
+            with open(path, 'wb') as f:
+                return pickle.load(f)
--- mainGUI.py
+++ mainGUI.py
@@ -1,11 +1,12 @@
 import numpy as np
 from GUI_consts import *
-from CommonFunctions import get_image_size
+from CommonFunctions import get_image_size, MyDict, stderr
 from ImageProperties import ImageObject, ImageProperties
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.uix.widget import Widget
+from kivy.graphics import Color, Line, Rectangle
 from kivy.core.window import Window, WindowBase
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
@@ -15,7 +16,7 @@ from os.path import isfile
 import warnings
 
 
-ImageObjects = []
+ImageObjects = ImageProperties(G_image_path)
 
 
 class ButtonsWidget(GridLayout):
@@ -34,7 +35,7 @@ class ButtonsWidget(GridLayout):
         self.motion_path_button = Button(text='Motion path(0)')
         self.calculate_scene_button = Button(text='Calculate Scene')
         self.render_scene_button = Button(text='Render Scene')
-        self.widgets = {}
+        self.widgets = MyDict()
         self.buttons = (self.path_input, self.load_data_button, self.ground_button,
                         self.sky_button, self.motion_path_button, self.quit_button, self.open_button,
                         self.save_data_button, self.walls_button,
@@ -52,27 +53,52 @@ class ButtonsWidget(GridLayout):
 
         self.in_process_action_type = None
         self.current_points = []
+        self.current_touches = []
 
-    def add_touch(self, point):
+        self.prev_draw_point = None
+        self.colors = MyDict.from_dictionary({'Ground': (1, 0, 0),
+                                              'Walls': (1, 1, 0),
+                                              'Sky': (0, 0, 1),
+                                              'Motion': (0, 1, 0)})
+        print 'colors:', self.colors
+
+    def add_point(self, point):
         if self.in_process_action_type is None:
             return
         self.current_points.append(point)
 
+    def add_touch(self, touch):
+        if self.in_process_action_type is None:
+            return
+        self.current_touches.append(touch)
+
     def add_object(self):
-        if self.in_process_action_type is not None and self.current_points:
-                ImageObjects.append(ImageObject(points=self.current_points, type=self.in_process_action_type))
+        if self.in_process_action_type is not None and self.current_points and self.current_points.__len__() > 2:
+            ImageObjects.add(ImageObject(points=self.current_points, type=self.in_process_action_type))
+            print 'Object added: %s %s' % (self.in_process_action_type, self.current_points.__str__())
         self.in_process_action_type = None
         self.current_points = []
+        self.prev_draw_point = None
+
+    def draw_point(self, touch):
+        if not self.in_process_action_type:
+            self.prev_draw_point = None
+            return
+        if self.prev_draw_point:
+            image_widget.draw_line(self.prev_draw_point, touch, self.colors[self.in_process_action_type])
+        self.prev_draw_point = touch
 
     def on_open_button_pressed(self):
         image_widget.source = self.widgets['TextInput'].text
         image_widget.size = get_image_size(image_widget.source)
+        ImageObjects.copy(ImageProperties(image_widget.source))
 
     def on_calculate_button_pressed(self):
         pass
 
     def on_motion_button_pressed(self):
-        pass
+        self.add_object()
+        self.in_process_action_type = 'Motion'
 
     def on_load_button_pressed(self):
         pass
@@ -81,19 +107,19 @@ class ButtonsWidget(GridLayout):
         pass
 
     def on_clear_button_pressed(self):
-        pass
+        self.in_process_action_type = None
 
     def on_ground_button_pressed(self):
-        print 'lala'
-        print ImageObjects[0].show() if ImageObjects else 'Empty'
         self.add_object()
         self.in_process_action_type = 'Ground'
 
     def on_walls_button_pressed(self):
-        pass
+        self.add_object()
+        self.in_process_action_type = 'Walls'
 
     def on_sky_button_pressed(self):
-        pass
+        self.add_object()
+        self.in_process_action_type = 'Sky'
 
     def on_render_button_pressed(self):
         pass
@@ -113,19 +139,25 @@ class ImageWidget(Image):
         if not self.collide_point(touch.x, touch.y):
             return
         print self.collide_point(touch.x, touch.y)
-        buttons_widget.add_touch(self.get_image_point((touch.x, touch.y)))
+        buttons_widget.add_touch(touch)
+        buttons_widget.draw_point(touch)
+        buttons_widget.add_point(self.get_image_point((touch.x, touch.y)))
+
+    def draw_line(self, touch1, touch2, (r, g, b)):
+        print >>stderr, 'DRAW!'
+        with self.canvas:
+            Color(r, g, b)
+            Line(points=touch1.pos + touch2.pos, width=5)
+
 
     def get_image_point(self, (x, y)):
         win_size = self.size
         img_size = map(float, get_image_size(self.source))
-        # print win_size, img_size, self.size
         win_prop, img_prop = win_size[0] / win_size[1], img_size[0] / img_size[1]
-        # print win_prop, img_size
         if win_prop > 1 and img_prop > 1:
             t = float(win_size[1]) / img_size[1]
             ans_y = y / t
             ans_x = (x - (win_size[0] - img_size[0] * t) * .5) / t
-            # print 'on Image: %f %f' % (ans_x, ans_y)
             return ans_x, ans_y
         return 0, 0
 
